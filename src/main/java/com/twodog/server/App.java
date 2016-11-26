277ba06040e3f008d1c3de5e8c034cb2ec1117b7
husu1014 <husu@192.168.0.103>
2016-11-27 00:15:21 +0800

    init project

---
+++ src/main/java/com/twodog/server/App.java
@@ -0,0 +1,13 @@
+package com.twodog.server;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
